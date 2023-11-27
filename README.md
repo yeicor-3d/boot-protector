ac280cb37ef590c6209ad40ad7f10b97c8f738cd
Yeicor <4929005+Yeicor@users.noreply.github.com>
2023-11-27 20:35:06 +0100

    Initial commit of new project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boot protector
+
+You can download the latest builds from the [workflow runs](https://github.com/Yeicor/boot-protector/actions/workflows/main.yml).
+
+You can preview the model in an interactive demo by clicking the following render of the latest design:
+
+[![boot-protector.png](https://yeicor.github.io/boot-protector/models/main/boot-protector.png)](https://yeicor.github.io/boot-protector/)
+
+![boot-protector.svg](https://yeicor.github.io/boot-protector/models/main/boot-protector.svg)
---
+++ main.py
@@ -0,0 +1,39 @@
+from build123d import *
+from contextlib import suppress
+with suppress(ImportError): import ocp_vscode # Optional, for visualizing the model in VSCode instead of CQ-editor or exporting to STL
+
+
+# ================== PARAMETERS ==================
+# 3D printing basics
+tol = 0.1 * MM  # Tolerance (tighter than usual)
+wall_min = 0.4 * MM  # Minimum wall width
+wall = 3 * wall_min  # Recommended width for most walls of this print
+eps = 1e-5 * MM  # A small number
+
+# ...
+
+
+# ================== MODELLING ==================
+
+with BuildPart() as obj:
+  Box(100*MM, 100*MM, 20*MM)
+
+# ================== SHOWING/EXPORTING ==================
+
+export = True
+try:
+    if 'ocp_vscode' in locals():
+        import socket
+        tmp_socket = socket.socket()
+        tmp_socket.connect(('localhost', ocp_vscode.get_port()))
+        tmp_socket.close()
+        ocp_vscode.reset_show()
+        ocp_vscode.show_all()
+        export = False # If the connection fails, export to STL instead
+    elif 'show_object' in locals():
+        show_object(obj, 'boot-protector') # type: ignore
+except Exception as ex:
+    print("Cannot show model, exporting to STL instead (%s)" % ex)
+    
+if export:
+    obj.export_stl('boot-protector.stl')
