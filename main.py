346eb630fba879ced4a32aaaa3b9514310c12bcf
Yeicor <4929005+Yeicor@users.noreply.github.com>
2023-12-09 20:40:14 +0100

    Testing...

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ final_model = getenv("final") is not None
 # Protection parameters
 prot_clip_x = -6 * MM
 prot_clip_x_radius = 6 * CM
-prot_clip_z_offset = 4 * MM  # From automatic Z cut
+prot_clip_z_offset = 25 * MM
+prot_clip_z_rotation = (0, 0, 0)
 prot_z_extra = 8 * MM  # Vertical wall to add below cut
+prot_offset = 1 * MM  # Offset of the protection from the boot
 prot_thickness = 2 * wall  # Thickness of the protection (external)
 
 # Sewing parameters
@@ -30,13 +32,42 @@ sew_hole_sep = 2 * 2 * sew_hole_radius
 
 # Load the boot model, which must be aligned with the base at the origin, centered in X and Y and with forward direction in X+
 # Slow import...
-boot = Mesher().read("boot-0.stl" if final_model else "boot-0-simpl.stl")[0]
+# boot = Mesher().read("boot-0.stl" if final_model else "boot-0-simpl.stl")
+boot = import_step("boot-0-simpl.step")
+# assert len(boot) == 1
+# boot = boot[0]
+boot.fix()
+
+if 'ocp_vscode' in locals():
+    ocp_vscode.show_all()
+assert boot.is_valid()
+print(boot)
+boot = boot.solid()
 boot_bb = boot.bounding_box()
 
 # %%
 
 with BuildPart() as obj:
     add(boot)
+
+    with Locations(Location((prot_clip_x-prot_clip_x_radius, boot_bb.center().Y, boot_bb.min.Z))):
+        Box(boot_bb.size.X/2, boot_bb.size.Y, boot_bb.size.Z, align=(
+            Align.MAX, Align.CENTER, Align.MIN), mode=Mode.SUBTRACT)
+        Cylinder(prot_clip_x_radius, boot_bb.size.Z, align=(
+            Align.CENTER, Align.CENTER, Align.MIN), mode=Mode.SUBTRACT)
+
+    with Locations(Location((boot_bb.min.X, boot_bb.min.Y, 0))):
+        Box(boot_bb.size.X, boot_bb.size.Y, prot_clip_z_offset,
+            align=Align.MIN, mode=Mode.SUBTRACT)
+
+    assert obj.part.is_valid()
+    back_faces = faces().filter_by(lambda f: f.normal_at(
+        None).Z < eps and f.normal_at(None).X < eps and abs(f.normal_at(None).Y) < 0.4)
+    op = back_faces + [faces().sort_by(Axis.Z)[-1]]
+    # BROKEN!!! offset(openings=op, kind=Kind.INTERSECTION)
+
+if 'ocp_vscode' in locals():
+    ocp_vscode.show_all()
 
 # %%
 # ================== SHOWING/EXPORTING ==================
@@ -49,11 +80,12 @@ try:
     elif "ocp_vscode" in locals():
         ocp_vscode.reset_show()
         ocp_vscode.show_all()
-        export = False
+        # export = False
 except Exception as ex:
     print("Cannot show model, exporting to STL instead (%s)" % ex)
 
 if export:
+    print("Exporting to STL")
     obj.part.export_stl("boot-protector.stl")
 
 # %%
